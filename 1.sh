10040d2f1600a6ac246d16a1088e78dc07826626
AdrianAmore <58187524+AdrianAmore@users.noreply.github.com>
2020-12-21 19:01:11 +0100

    Subiendo 1.sh

---
+++ 1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+maxLineas=`cat nombres.txt | wc -l`
+linea=1
+
+while [ $linea -le $maxLineas ]; do
+	nombre=`cat nombres.txt | head -$linea | tail -1`
+	mkdir "$nombre"
+	cd "$nombre"
+	for i in `seq 1 $1`
+	do
+		mkdir personal$i
+	done
+	cd ..
+	linea=$((linea+1))
+done
